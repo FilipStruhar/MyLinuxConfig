eab48ae038f7dfe5abfd116e1bcf69be9d9ed413
FilipStruhar <struhar.f@email.cz>
2024-11-14 13:54:47 +0100

    hack nerdfont setup debug

--- nerdfonts/HackFontSetup.sh
+++ nerdfonts/HackFontSetup.sh
@@ -1,7 +1,7 @@
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip -o ~/Downloads/Hack.zip
 
-unzip ~/Hack.zip -d ~/hack_font
+unzip ~/Downloads/Hack.zip -d ~/Downloads/hack_font
 
 cp ~/Downloads/hack_font/*.ttf ~/.local/share/fonts/
 
